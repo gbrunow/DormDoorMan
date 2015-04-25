7b78de6ec434091114aeb05dee10044fca06cbfa
Guilherme Brunow <gbrunowgomes@crimson.ua.edu>
2015-04-25 14:52:28 -0500

    First Commit

---
+++ DormDoorMan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DormDoorMan.Startup))]
+namespace DormDoorMan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
